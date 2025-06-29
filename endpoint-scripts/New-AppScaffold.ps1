1549e79a3eaea268661a3ba99450f9b7fd9bf06a
mattmcphee <matt.imcphee@gmail.com>
2025-06-30 09:42:47 +1000

    script changes to support psadt 4.0

--- endpoint-scripts/New-AppScaffold.ps1
+++ endpoint-scripts/New-AppScaffold.ps1
@@ -20,6 +20,13 @@ function New-AppScaffold {
     New-Item -Path "$Path\$ApplicationName" -Name "Install" -ItemType Directory
     New-Item -Path "$Path\$ApplicationName" -Name "Uninstall" -ItemType Directory
 
-    Invoke-Expression -Command 'robocopy "C:\sources\staging\bmd-psadt" "$Path\$ApplicationName\Install" /e'
-    Invoke-Expression -Command 'robocopy "C:\sources\staging\bmd-psadt" "$Path\$ApplicationName\Uninstall" /e'
+    Copy-Item -Path 'C:\sources\staging\bmd-psadt-4\*' `
+        -Destination "$Path\$ApplicationName\Install" `
+        -Recurse `
+        -Force
+
+    Copy-Item -Path 'C:\sources\staging\bmd-psadt-4\*' `
+        -Destination "$Path\$ApplicationName\Uninstall" `
+        -Recurse `
+        -Force
 }
---
+++ endpoint-scripts/New-SCCMApplication4.ps1
@@ -0,0 +1,171 @@
+function New-SCCMApplication4 {
+    param (
+        # ApplicationName
+        [Parameter(Mandatory=$true)]
+        [string]
+        $ApplicationName,
+        # Publisher
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Publisher,
+        # Version
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Version,
+        # Owner
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Owner,
+        # Description
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Description,
+        # IconPath - path to icon file on network drive
+        [Parameter(Mandatory=$true)]
+        [string]
+        $IconPath,
+        # ContentLocation - path to content on network drive
+        [Parameter(Mandatory=$true)]
+        [string]
+        $ContentLocation,
+        # Free space required (MB)
+        [Parameter(Mandatory=$true)]
+        [string]
+        $SpaceRequired,
+        # Folder where application gets installed
+        [Parameter(Mandatory=$true)]
+        [string]
+        $InstallFolder,
+        # Name of the installed file, including file extension
+        [Parameter(Mandatory=$true)]
+        [string]
+        $InstalledFile,
+        # Registry key path (don't include root e.g. hklm or hkey_local_machine)
+        [Parameter(Mandatory=$true)]
+        [string]
+        $RegKeyPath,
+        # Registry key name
+        [Parameter(Mandatory=$true)]
+        [string]
+        $RegKeyName,
+        # Registry key value
+        [Parameter(Mandatory=$true)]
+        [string]
+        $RegKeyValue,
+        # is64bit - whether or not the application is 64bit
+        [Parameter(Mandatory=$false)]
+        [switch]
+        $Is64Bit,
+        # Max run time
+        [Parameter(Mandatory=$true)]
+        [string]
+        $MaxRunTime,
+        # Estimated run time
+        [Parameter(Mandatory=$true)]
+        [string]
+        $EstimatedRunTime,
+        # Uninstall content location
+        [Parameter(Mandatory=$false)]
+        [string]
+        $UninstallContentLocation
+    )
+
+    Set-Location -Path 'A00:\'
+
+    # if app doesn't exist, create it
+    if (-not(Get-CMApplication -Name $ApplicationName -Fast)) {
+        New-CMApplication `
+            -Owner $Owner `
+            -SupportContact $Owner `
+            -DefaultLanguageId 3081 <# en-AU #> `
+            -IconLocationFile $IconPath `
+            -LocalizedDescription $Description `
+            -LocalizedName $ApplicationName `
+            -Name $ApplicationName `
+            -Publisher $Publisher `
+            -SoftwareVersion $Version
+    }
+
+    $app = Get-CMApplication -Name $ApplicationName
+
+    # if publisher folder doesn't exist, create it
+    if (-not(Test-Path -Path ".\Application\$Publisher")) {
+        New-CMFolder -Name $Publisher -ParentFolderPath '.\Application'
+    }
+
+    # move app into folder
+    $app | Move-CMObject -FolderPath ".\Application\$Publisher"
+
+    # create requirement rules
+    $freeSpaceRule = Get-CMGlobalCondition -Name "Free disk space" |
+        New-CMRequirementRuleFreeDiskSpaceValue `
+            -PartitionOption 'System' `
+            -RuleOperator 'GreaterThan' `
+            -Value1 $SpaceRequired
+    $x64Rule = Get-CMGlobalCondition -Name "Supported Workstation OS x64" |
+        New-CMRequirementRuleBooleanValue -Value $true
+
+    # check to see if application has deployment type
+    # if app has no deployment types, create one
+    if ($app.NumberOfDeploymentTypes -lt 1) {
+        # file detection clause
+        if ($Is64Bit) {
+            $fileDetClause = New-CMDetectionClauseFile `
+            -FileName $InstalledFile `
+            -Path $InstallFolder `
+            -Existence `
+            -Is64Bit
+        } else {
+            $fileDetClause = New-CMDetectionClauseFile `
+            -FileName $InstalledFile `
+            -Path $InstallFolder `
+            -Existence
+        }
+
+        # registry detection clause
+        $regDetClause = New-CMDetectionClauseRegistryKeyValue `
+        -Hive 'LocalMachine' `
+        -KeyName $RegKeyPath `
+        -PropertyType 'String' `
+        -ValueName $RegKeyName `
+        -Value `
+        -ExpectedValue $RegKeyValue `
+        -ExpressionOperator 'IsEquals' `
+        -Is64Bit
+
+        # if uninstall location has been provided then set the uninstall
+        # if not then set same for install and uninstall
+        if ($UninstallContentLocation) {
+            Add-CMScriptDeploymentType `
+            -DeploymentTypeName $ApplicationName `
+            -ApplicationName $ApplicationName `
+            -InstallationBehaviorType 'InstallForSystem' `
+            -LogonRequirementType 'WhetherOrNotUserLoggedOn' `
+            -MaximumRuntimeMins $MaxRunTime `
+            -EstimatedRuntimeMins $EstimatedRunTime `
+            -AddRequirement @($freeSpaceRule,$x64Rule) `
+            -ContentLocation $ContentLocation `
+            -UninstallOption 'Different' `
+            -UninstallContentLocation $UninstallContentLocation `
+            -InstallCommand "Invoke-AppDeployToolkit.exe -AllowRebootPassThru -DeploymentType Install" `
+            -UninstallCommand "Invoke-AppDeployToolkit.exe -AllowRebootPassThru -DeploymentType Uninstall" `
+            -AddDetectionClause $fileDetClause,$regDetClause
+        } else {
+            Add-CMScriptDeploymentType `
+            -DeploymentTypeName $ApplicationName `
+            -ApplicationName $ApplicationName `
+            -InstallationBehaviorType 'InstallForSystem' `
+            -LogonRequirementType 'WhetherOrNotUserLoggedOn' `
+            -MaximumRuntimeMins $MaxRunTime `
+            -EstimatedRuntimeMins $EstimatedRunTime `
+            -AddRequirement @($freeSpaceRule,$x64Rule) `
+            -ContentLocation $ContentLocation `
+            -InstallCommand "Invoke-AppDeployToolkit.exe -AllowRebootPassThru -DeploymentType Install" `
+            -UninstallCommand "Invoke-AppDeployToolkit.exe -AllowRebootPassThru -DeploymentType Uninstall" `
+            -AddDetectionClause $fileDetClause,$regDetClause
+        }
+    } else {
+        # app already has deployment types, let's exit
+        throw -Message "This app already has at least one deployment type. Exiting..."
+    }
+}
