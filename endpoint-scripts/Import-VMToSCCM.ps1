9f794e25c2be2be48f5adc6f6c1ee34d682a4544
mattmcphee <matt.imcphee@gmail.com>
2025-07-24 15:12:10 +1000

    Added option to add SMBiosGuid

--- endpoint-scripts/Import-VMToSCCM.ps1
+++ endpoint-scripts/Import-VMToSCCM.ps1
@@ -7,12 +7,23 @@ function Import-VMToSCCM {
         # MacAddress
         [Parameter(Mandatory)]
         [string]
-        $MacAddress
+        $MacAddress,
+        # SMBiosGuid
+        [Parameter(Mandatory=$false)]
+        [string]
+        $SMBiosGuid
     )
 
     $OsdPromptCollectionId = "A000005E"
 
-    Import-CMComputerInformation -ComputerName $ComputerName `
-        -MacAddress $MacAddress `
-        -CollectionId $OsdPromptCollectionId
+    if ($SMBiosGuid) {
+        Import-CMComputerInformation -ComputerName $ComputerName `
+            -MacAddress $MacAddress `
+            -CollectionId $OsdPromptCollectionId `
+            -SMBiosGuid $SMBiosGuid
+    } else {
+        Import-CMComputerInformation -ComputerName $ComputerName `
+            -MacAddress $MacAddress `
+            -CollectionId $OsdPromptCollectionId
+    }
 }
