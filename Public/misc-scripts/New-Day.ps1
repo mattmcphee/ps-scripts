4aae0ad5eba296cd073b7bfaee4834ffc263c2a2
mattmcphee <matt.imcphee@gmail.com>
2025-12-18 10:12:23 +1000

    changed new-day script to use weekly notepad repo

--- Public/misc-scripts/New-Day.ps1
+++ Public/misc-scripts/New-Day.ps1
@@ -8,32 +8,19 @@ function New-Day {
         $Day
     )
 
-    $homeLaptop = "5cd3155227"
-    $workLaptop = "5cd5152rbl"
     $date = Get-Date -Format "yyyy-MM-dd"
     $dateLong = Get-Date -Format "dd-MMM-yyyy"
     $dateArr = $date -split "-"
     $dateYear = $dateArr[0]
     # $dateMonth = $dateArr[1]
     # $dateDay = $dateArr[2]
-    $workLaptopPath = "\\$workLaptop\C$\Users\matmcp1\OneDrive - B.M.D. Holdings Pty. Limited\Documents\weeklynotepad$dateYear\$date.txt"
-    $homeLaptopPath = "\\$homeLaptop\C$\Users\matmcp1\OneDrive - B.M.D. Holdings Pty. Limited\Documents\weeklynotepad$dateYear\$date.txt"
-    
-    if ($Day -like "Wednesday" -or $Day -like "Thursday") {
-        # home laptop
-        if (-not ($homeLaptopPath)) {
-            New-Item -Path $homeLaptopPath -Force
-        }
-        Get-BlockLetters $Day | Add-Content -Path $homeLaptopPath
-        Get-BlockLetters $dateLong | Add-Content -Path $homeLaptopPath
-        Add-Content -Path $homeLaptopPath -Value ""
-    } else {
-        # work laptop
-        if (-not ($workLaptopPath)) {
-            New-Item -Path $workLaptopPath -Force
-        }
-        Get-BlockLetters $Day | Add-Content -Path $workLaptopPath
-        Get-BlockLetters $dateLong | Add-Content -Path $workLaptopPath
-        Add-Content -Path $workLaptopPath -Value ""
+    $notePath = "$env:windir\sources\repos\weekly-notepad\weeklynotepad$dateYear\$date.txt"
+
+    if (-not ($notePath)) {
+        New-Item -Path $notePath -Force
     }
+
+    Get-BlockLetters $Day | Add-Content -Path $notePath
+    Get-BlockLetters $dateLong | Add-Content -Path $notePath
+    Add-Content -Path $notePath -Value ""
 }
