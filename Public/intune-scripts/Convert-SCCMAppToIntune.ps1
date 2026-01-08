fb8af2036a26bbaa503fdb27ed4bb316ffd335e1
mattmcphee <matt.imcphee@gmail.com>
2026-01-08 16:02:00 +1000

    bug fixes for intune app packaging scripts

---
+++ Public/intune-scripts/Convert-SCCMAppToIntune.ps1
@@ -0,0 +1,191 @@
+﻿function Convert-SCCMAppToIntune {
+    [CmdletBinding()]
+    param (
+        # ApplicationName - exact name as it appears in SCCM
+        [Parameter(Mandatory)]
+        [string]$ApplicationName,
+        
+        # IconPath - path to image icon
+        [Parameter(Mandatory)]
+        [ValidateScript({
+            if (-not (Test-Path -Path $_ -PathType Leaf)) {
+                throw "$_ not found or is a folder instead of a file."
+            } elseif ($_ -notlike "*.png" -and $_ -notlike "*.jpg") {
+                throw "$_ is not a valid image file path ending in png or jpg."
+            } else {
+                return $true
+            }
+        })]
+        [string]$IconPath,
+
+        # InstalledApplicationSizeMB
+        [Parameter(Mandatory)]
+        [int]$InstalledApplicationSizeMB,
+
+        # Developer - author of the application (optional)
+        [Parameter(Mandatory = $false)]
+        [string]$Developer,
+
+        # Owner - product owner of the application within the organization (optional)
+        [Parameter(Mandatory = $false)]
+        [string]$Owner,
+
+        # Notes - optional
+        [Parameter(Mandatory = $false)]
+        [string]$Notes,
+
+        # InformationURL - link to a knowledge base item with further info (optional)
+        [Parameter(Mandatory = $false)]
+        [string]$InformationURL,
+
+        # PrivacyURL - link to the app's privacy policy (optional)
+        [Parameter(Mandatory = $false)]
+        [string]$PrivacyURL,
+
+        # CompanyPortalFeaturedApp - whether to feature the app in company portal (optional, defaults to false)
+        [Parameter(Mandatory = $false)]
+        [bool]$CompanyPortalFeaturedApp = $false,
+
+        # CategoryName - specify a single or multiple categories to categorize the app (optional)
+        [Parameter(Mandatory = $false)]
+        [string[]]$CategoryName,
+        
+        # RestartBehavior - restart behavior if app requires restart (optional, defaults to basedOnReturnCode)
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("allow", "basedOnReturnCode", "suppress", "force")]
+        [string]$RestartBehavior = "basedOnReturnCode",
+
+        # InstallExperience - install as system or user (optional, default SYSTEM)
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("SYSTEM", "User")]
+        [string]$InstallExperience = "SYSTEM",
+
+        # InstallCommandLine - install command line (optional)
+        [Parameter(Mandatory = $false)]
+        [string]$InstallCommandLine,
+
+        # UninstallCommandLine - uninstall command line (optional)
+        [Parameter(Mandatory = $false)]
+        [string]$UninstallCommandLine
+    )
+    
+    # get app
+    try {
+        $ogLoc = Get-Location
+        Set-Location "A00:"
+        $app = Get-CMApplication -Name $ApplicationName
+        Set-Location $ogLoc
+    } catch {
+        throw "Could not find application with that name: $_"
+    }
+
+    # pull out info from sdmpackagexml
+    [xml]$appxml = $app.SDMPackageXml
+    $appxmlDisplayInfo = $appxml.AppMgmtDigest.Application.DisplayInfo.Info
+    $appXmlCustomData = $appxml.AppMgmtDigest.DeploymentType.Installer.CustomData
+
+    $displayName = $appxmlDisplayInfo.Title
+    $description = $appxmlDisplayInfo.Description
+    $publisher = $appxmlDisplayInfo.Publisher
+    $appVersion = $appxmlDisplayInfo.Version
+
+    # base folder and installer file are two separate elements in xml
+    $installerFile = $appXmlCustomData.InstallCommandLine -split " " |
+        Select-Object -First 1
+    $sourcePathBase = $appxml.AppMgmtDigest.DeploymentType.Installer.Contents.Content |
+        Where-Object { $_.Location -notlike "*\Uninstall\*" } |
+        Select-Object -ExpandProperty Location
+    $sourcePath = $sourcePathBase + $installerFile
+
+    # app installation folder and app exe are two separate elements
+    $appPathBase = $appXmlCustomData.EnhancedDetectionMethod.Settings.File.Path
+    $appPathExe = $appXmlCustomData.EnhancedDetectionMethod.Settings.File.Filter
+    if ([string]::IsNullOrEmpty($appPathBase) -or [string]::IsNullOrEmpty($appPathExe)) {
+        throw "Unable to find file existence detection method for: $($app.LocalizedDisplayName)"
+    }
+    $applicationExePath = "$appPathBase\$appPathExe"
+
+    # reg key path and reg key value are two separate elements
+    $regKeyPath = "HKEY_LOCAL_MACHINE\" + $appXmlCustomData.EnhancedDetectionMethod.Settings.SimpleSetting.RegistryDiscoverySource.Key
+    $displayVersion = $appXmlCustomData.EnhancedDetectionMethod.Rule.Expression.Operands.Expression.Operands.ConstantValue |
+        Where-Object { $_.DataType -like "String" } | 
+        Select-Object -ExpandProperty Value
+    if ([string]::IsNullOrEmpty($regKeyPath) -or [string]::IsNullOrEmpty($displayVersion)) {
+        throw "Unable to find registry key detection method for: $($app.LocalizedDisplayName)"
+    }
+
+    if ($PSBoundParameters["InstallCommandLine"]) {
+        $installCommandLine = $InstallCommandLine
+    } else {
+        $installCommandLine = $appXmlCustomData.InstallCommandLine
+    }
+
+    if (-not $installCommandLine) {
+        throw "Could not find install command line: $_"
+    }
+
+    if ($PSBoundParameters["UninstallCommandLine"]) {
+        $uninstallCommandLine = $UninstallCommandLine
+    } else {
+        $uninstallCommandLine = $appXmlCustomData.UninstallCommandLine
+    }
+
+    if (-not $UninstallCommandLine) {
+        throw "Could not find uninstall command line: $_"
+    }
+
+    # build splat
+    $newIntuneAppArgs = @{
+        SourcePath                  = $sourcePath
+        InstalledApplicationSizeMB  = $InstalledApplicationSizeMB
+        DisplayName                 = $displayName
+        Publisher                   = $publisher
+        Description                 = $description
+        ApplicationExePath          = $applicationExePath
+        RegKeyPath                  = $regKeyPath
+        DisplayVersion              = $displayVersion
+        IconPath                    = $IconPath
+        AppVersion                  = $appVersion
+        InstallCommandLine          = $installCommandLine
+        UninstallCommandLine        = $uninstallCommandLine
+    }
+
+    if ($PSBoundParameters["Developer"]) {
+        $newIntuneAppArgs.Add("Developer", $Developer)
+    }
+
+    if ($PSBoundParameters["Owner"]) {
+        $newIntuneAppArgs.Add("Owner", $Owner)
+    }
+
+    if ($PSBoundParameters["Notes"]) {
+        $newIntuneAppArgs.Add("Notes", $Notes)
+    }
+
+    if ($PSBoundParameters["InformationURL"]) {
+        $newIntuneAppArgs.Add("InformationURL", $InformationURL)
+    }
+
+    if ($PSBoundParameters["PrivacyURL"]) {
+        $newIntuneAppArgs.Add("PrivacyURL", $PrivacyURL)
+    }
+
+    if ($PSBoundParameters["CompanyPortalFeaturedApp"]) {
+        $newIntuneAppArgs.Add("CompanyPortalFeaturedApp", $CompanyPortalFeaturedApp)
+    }
+
+    if ($PSBoundParameters["RestartBehavior"]) {
+        $newIntuneAppArgs.Add("RestartBehavior", $RestartBehavior)
+    }
+
+    if ($PSBoundParameters["InstallExperience"]) {
+        $newIntuneAppArgs.Add("InstallExperience", $InstallExperience)
+    }
+
+    # create app
+    try {
+        New-IntuneApp @newIntuneAppArgs
+    } catch {
+        throw "Error encountered when creating app in Intune: $_"
+    }
+}
