e0a432cc8ce74621e1dcebcd3579763ca49d8641
mattmcphee <matt.imcphee@gmail.com>
2026-03-17 11:41:18 +1000

    Changed directory from C:\sources\staging to C:\windows\imecache

---
+++ Public/intune-scripts/Copy-InstallScripts.ps1
@@ -0,0 +1,30 @@
+﻿function Copy-InstallScripts {
+    [Alias("cis")]
+    param(
+        # TestMachineName
+        [Parameter(Mandatory)]
+        [string]$TestMachineName,
+        # Publisher
+        [Parameter(Mandatory)]
+        [string]$Publisher,
+        # ApplicationName
+        [Parameter(Mandatory)]
+        [string]$ApplicationName
+    )
+
+    $scripts = Get-ChildItem -Path "C:\sources\repos\matmcp1-psadt-app-scripts\$ApplicationName" -Recurse -Include "*.ps1"
+    $scriptDirectories = $scripts.FullName | Split-Path -Parent
+
+    foreach ($dir in $scriptDirectories) {
+        try {
+            Write-Verbose "Copying: '$dir' to 'C:\sources\staging'"
+            Copy-Item -Path $dir -Destination "C:\sources\staging" -Recurse -Force
+            Write-Verbose "Copying: '$dir' to '\\$TestMachineName\c$\windows\imecache'"
+            Copy-Item -Path $dir -Destination "\\$TestMachineName\c$\windows\imecache" -Recurse -Force
+            Write-Verbose "Copying: '$dir' to '\\bmd\bmdapps\sccm_packages\software\$Publisher'"
+            Copy-Item -Path $dir -Destination "\\bmd\bmdapps\sccm_packages\software\$Publisher" -Recurse -Force
+        } catch {
+            throw $_
+        }
+    }
+}
