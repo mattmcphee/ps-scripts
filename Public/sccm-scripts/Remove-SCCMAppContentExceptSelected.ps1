a804141ce85c7edd58e11b95355287a84334a098
mattmcphee <matt.imcphee@gmail.com>
2026-05-20 08:35:38 +1000

    made out-gridview title wording clearer

--- Public/sccm-scripts/Remove-SCCMAppContentExceptSelected.ps1
+++ Public/sccm-scripts/Remove-SCCMAppContentExceptSelected.ps1
@@ -47,7 +47,7 @@ function Remove-SCCMAppContentExceptSelected {
 
     $appsToKeep = $allApps | 
         Select-Object LocalizedDisplayName, SoftwareVersion, Manufacturer, DateCreated, CreatedBy, DateLastModified, LastModifiedBy | 
-        Out-GridView -Title "Select apps to KEEP (Unselected apps will have content DELETED from ALL DPs!)" -PassThru
+        Out-GridView -Title "Select app content to KEEP (Unselected apps will have content DELETED from ALL DPs!)" -PassThru
 
     # handle no selection
     if (-not $appsToKeep) {
