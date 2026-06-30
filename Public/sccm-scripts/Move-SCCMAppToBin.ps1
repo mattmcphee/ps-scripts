61b644cd8d68a9712af1cc6ce65b00d7895b09f2
mattmcphee <matt.imcphee@gmail.com>
2026-06-30 10:33:45 +1000

    new move sccm app to bin function

---
+++ Public/sccm-scripts/Move-SCCMAppToBin.ps1
@@ -0,0 +1,22 @@
+﻿function Move-SCCMAppToBin {
+    param (
+        # Name - name of application in sccm
+        [Parameter(Mandatory)]
+        [string]$Name
+    )
+
+    $ogLoc = Get-Location
+    Set-Location 'A00:\'
+
+    $app = Get-CMApplication -Name $Name -Fast
+
+    foreach ($application in $app) {
+        try {
+            $application | Move-CMObject -FolderPath ".\Application\``BIN"
+        } catch {
+            throw "Encountered error when moving application '$($application.LocalizedDisplayName)' to folder '.\Application\``BIN'. $_"
+        }
+    }
+
+    Set-Location $ogLoc
+}
