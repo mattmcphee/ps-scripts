56ed17f3a07892a1979bc4db91ed9544d6cb07c0
mattmcphee <matt.imcphee@gmail.com>
2026-06-30 13:23:44 +1000

    remove app content function

---
+++ Public/sccm-scripts/Remove-SCCMAppContent.ps1
@@ -0,0 +1,19 @@
+﻿function Remove-SCCMAppContent {
+    param (
+        # Name - name of app to remove all distributed content for
+        [Parameter(Mandatory)]
+        [string]$Name
+    )
+
+    $ogLoc = Get-Location
+    Set-Location 'A00:\'
+
+    $app = Get-CMApplication -Name $Name -Fast
+    $allDps = (Get-CMDistributionPoint).NetworkOSPath.Replace('\\', '')
+    $allDpGroups = (Get-CMDistributionPointGroup).Name
+
+    Remove-CMContentDistribution -ApplicationName $app.LocalizedDisplayName -DistributionPointGroupName $allDpGroups -Force -ErrorAction SilentlyContinue
+    Remove-CMContentDistribution -ApplicationName $app.LocalizedDisplayName -DistributionPointName $allDps -Force -ErrorAction SilentlyContinue
+
+    Set-Location $ogLoc
+}
