0f5d36b3086ca6bc8ad62ca075d2eeb8aae7c60f
mattmcphee <matt.imcphee@gmail.com>
2026-09-07 08:38:55 +1000

    bug fixes

--- Public/intune-scripts/Get-IntuneApp.ps1
+++ Public/intune-scripts/Get-IntuneApp.ps1
@@ -4,41 +4,57 @@
         [Parameter(Mandatory=$false)]
         [string]$DisplayName,
 
+        # ID - one or more app guid's to lookup
+        [Parameter(Mandatory=$false, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+        [string[]]$ID,
+
         # AllProperties - will add every app property to the returned objects
         [Parameter(Mandatory=$false)]
         [switch]$AllProperties
     )
 
-    $uri = "https://graph.microsoft.com/v1.0/deviceAppManagement/mobileApps"
+    begin {
+        $baseUri = "https://graph.microsoft.com/v1.0/deviceAppManagement/mobileApps"
+        $select = if ($AllProperties) { '' } else { '?$select=id,displayName' }
+        $results = [System.Collections.Generic.List[PSCustomObject]]::new()
 
-    # only get the basic fields
-    if (-not $AllProperties) {
-        $uri += '?$select=id,displayName'
-    }
-
-    $results = [System.Collections.Generic.List[PSCustomObject]]::new()
-
-    while ($uri) {
-        $response = Invoke-MgGraphRequest -Method GET -Uri $uri
-
-        foreach ($app in $response.value) {
-            if ($DisplayName -and $app.displayName -notlike $DisplayName) {
-                continue
-            }
+        function Add-AppObjectToList {
+            param([object]$App)
 
             if ($AllProperties) {
-                $results.Add([PSCustomObject]$app)
+                $results.Add([PSCustomObject]$App)
             } else {
                 $results.Add([PSCustomObject]@{
-                    ID                  = $app.id
-                    DisplayName         = $app.displayName
+                    ID          = $app.id
+                    DisplayName = $app.displayName
                 })
             }
         }
-
-        # Move to next page if results exceed the default page size
-        $uri = $response.'@odata.nextLink'
     }
 
-    $results
+    process {
+        if ($ID) {
+            foreach ($appId in $ID) {
+                $uri = "$baseUri/$appId$select"
+                $app = Invoke-MgGraphRequest -Method GET -Uri $uri -ErrorAction Stop
+                Add-AppObjectToList -App $app
+            }
+        } else {
+            $uri = "$baseUri$select"
+            while ($uri) {
+                $response = Invoke-MgGraphRequest -Method GET -Uri $uri -ErrorAction Stop
+                foreach ($app in $response.value) {
+                    if ($DisplayName -and $app.displayName -notlike $DisplayName) {
+                        continue
+                    }
+                    Add-AppObjectToList -App $app
+                }
+                $uri = $response.'@odata.nextLink'
+            }
+        }
+    }
+
+    end {
+        $results
+    }
 }
