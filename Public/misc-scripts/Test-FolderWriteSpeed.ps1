c313f601f1422d89ad7c89cd2c24de638c3fc2fa
mattmcphee <matt.imcphee@gmail.com>
2026-09-18 07:47:05 +1000

    new test folder write speed function

---
+++ Public/misc-scripts/Test-FolderWriteSpeed.ps1
@@ -0,0 +1,40 @@
+﻿function Test-FolderWriteSpeed {
+    param (
+        [Parameter(Mandatory)]
+        [string]$Path,
+
+        [Parameter(Mandatory=$false)]
+        [int]$SizeMB = 100
+    )
+
+    $TestFile = Join-Path $Path "SpeedTest_$([guid]::NewGuid()).tmp"
+    $Buffer   = New-Object byte[] (1MB)
+    $Watch    = [System.Diagnostics.Stopwatch]::StartNew()
+
+    try {
+        $Stream = [System.IO.File]::Create($TestFile)
+
+        for ($i = 0; $i -lt $SizeMB; $i++) {
+            $Stream.Write($Buffer, 0, $Buffer.Length)
+        }
+
+        $Stream.Flush()
+        $Stream.Close()
+
+        $Watch.Stop()
+
+        [pscustomobject]@{
+            Path     = $Path
+            SizeMB   = $SizeMB
+            Seconds  = [math]::Round($Watch.Elapsed.TotalSeconds, 2)
+            'MB/s'     = [math]::Round($SizeMB / $Watch.Elapsed.TotalSeconds, 2)
+        }
+    }
+    finally {
+        if ($Stream) {
+            $Stream.Dispose()
+        }
+
+        Remove-Item $TestFile -Force -ErrorAction SilentlyContinue
+    }
+}
